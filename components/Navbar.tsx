06636c7cec069af4577278a8d41c7a39a59e9c0b
raul <raul.alexander.floress@gmail.com>
2023-04-17 16:58:08 -0500

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,8 @@ import { FC } from "react"
 import styles from "../styles/Home.module.css"
 import dynamic from "next/dynamic";
 
+// Navbar added
+
 const WalletMultiButtonDynamic = dynamic(
 	async () =>
 		(await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
