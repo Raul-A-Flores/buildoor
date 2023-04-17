7eed46a1eeb6e5b13253a99b8491f50f1369f7cc
raul <raul.alexander.floress@gmail.com>
2023-04-17 17:02:21 -0500

    Nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Box, Center, Spacer, Stack } from "@chakra-ui/react"
 import type { NextPage } from "next"
 import Head from "next/head"
 import styles from "../styles/Home.module.css"
-import NavBar from "../components/NavBar"
+import Navbar from "../components/Navbar"
 import Disconnected from '../components/Disconnected'
 import { useWallet } from "@solana/wallet-adapter-react"
 import Connected from "../components/Connected"
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
       >
         <Stack w="full" h="calc(100vh)" justify="center">
 					{ /* NavBar */ }
-          <NavBar />
+          <Navbar />
           <Spacer />
           <Center>
           {connected ? <Connected /> : <Disconnected />}
